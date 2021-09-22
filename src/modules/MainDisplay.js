edc18a946a4ea87b4561a5e6b040fe5f120decfd
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-09-22 18:47:18 +0500

    Create displayProjects()

--- src/modules/MainDisplay.js
+++ src/modules/MainDisplay.js
@@ -1,12 +1,29 @@
+import DOMFactory from "./FactoryFunctions.js";
 import { pubsub } from "./Pubsub.js";
 
 function getData() {
     pubsub.subscribe('addTask', log);
     pubsub.subscribe('addProject', log)
+    pubsub.subscribe('addProject', displayProjects)
 }
 
 function log(data) {
     console.log(data);
+}
+
+function displayProjects(projects) {        //  NEED A CHECK FOR DUPLICATE PROJECTS IN Project.js
+    projects = projects.filter(project => {
+        if (project.title === "") return false
+        if (document.querySelector(`#${project.title}`)) return false
+        return true
+    })
+    for (const project of projects) {
+        const projectName = DOMFactory('h3', {id: `${project.title}`, className: "projectName",
+                                              textContent: `${project.title}`});
+        const addTaskInProjectButton = DOMFactory('button', {id: `${project.title} button`, 
+                                                             textContent: "Add Task in Project"});
+        document.body.append(projectName, addTaskInProjectButton);
+    }
 }
 
 export default getData;
